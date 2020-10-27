f57f61b3a052654b3d9324e37b8cb886c9b703f2
alexander-svirzhevsky <alex-ander.1@mail.ru>
2020-10-27 14:13:22 +0300

    init

--- projects/dom/index.js
+++ projects/dom/index.js
@@ -11,6 +11,9 @@
    createDivWithText('loftschool') // создаст элемент div, поместит в него 'loftschool' и вернет созданный элемент
  */
 function createDivWithText(text) {
+
+  document.body.appendChild(text);
+  text.textContent = 'loft';
 }
 
 /*
